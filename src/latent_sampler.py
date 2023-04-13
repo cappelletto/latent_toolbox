f619eba54011ae987ca1c6027257e9a8024c3a44
cappelletto <cappelletto@gmail.com>
2023-04-13 19:39:26 +0100

    Finish latent/labe sampler with ALL option. Regex filter for columns key added. Speed improvement with key preselection

--- src/latent_sampler.py
+++ src/latent_sampler.py
@@ -65,6 +65,15 @@ def main(args=None):
         help="Distance threshold that defines the maximum distance between the SOURCE and TARGET entries to be considered a match. Set to negative value if you do not want a limit."
     )
 
+    # mode #########################
+    parser.add_argument(
+        "-m",
+        "--mode",
+        default='closest',
+        type=str,
+        help="Mode of the sampler. Options are: 'closest' (default), 'random', 'all'."
+    )
+
     # parse arguments
     args = parser.parse_args(args)
     print (args)
@@ -99,13 +108,22 @@ def main(args=None):
 
     # Check if the provided key exists in the SOURCE file
     if args.key is not None:
-        if args.key not in df_source.columns:
+        # Check if the provided key exists in the df_source.columns using regex for string expansion (e.g. 'key' will match 'key_1', 'key_2', etc.)
+        if not any(args.key in s for s in df_source.columns):
             print ("Provided key: [" + args.key + "] not found in SOURCE file.")
             exit()
         else:
             print ("Provided key: [" + args.key + "] found in SOURCE file.")
 
-    # CHeck if the SOURCE dataframe contains the UTM fields: 'northing_utm [m]' and 'easting_utm [m]'
+        # If the key does not exist, the script will exit
+
+        # if args.key not in df_source.columns:
+        #     print ("Provided key: [" + args.key + "] not found in SOURCE file.")
+        #     exit()
+        # else:
+        #     print ("Provided key: [" + args.key + "] found in SOURCE file.")
+
+    # Check if the SOURCE dataframe contains the UTM fields: 'northing_utm [m]' and 'easting_utm [m]'
     # We could calculate them, but for now we will assume that they are already there. External tools to convert to UTM is already provided twice
     if 'northing_utm [m]' not in df_source.columns:
         print ("SOURCE file does not contain the northing_utm [m] field.")
@@ -131,6 +149,12 @@ def main(args=None):
     df_results = pd.DataFrame(columns=df_target.columns)
     # Iterate over the TARGET entries
     try:
+        # the keys to append are known in advance
+        if args.key is not None:
+            fields_to_append = df_source.filter(regex=args.key + "*").columns
+        else:
+            fields_to_append = df_source.columns
+
         for index, row in df_target.iterrows():
             # Print the number of the current entry being processed
             # print ("Processing TARGET entry: " + str(index))
@@ -138,36 +162,45 @@ def main(args=None):
             df_source['distance'] = np.sqrt((df_source['northing_utm [m]'] - row['northing_utm [m]'])**2 + (df_source['easting_utm [m]'] - row['easting_utm [m]'])**2)
             # Sort the SOURCE entries by distance
             df_source = df_source.sort_values(by=['distance'])
-            # Get the first entry (closest match)
-            closest_match = df_source.iloc[0]
-            # If the distance is less than the provided threshold, then append the closest match to the TARGET entry
-            # If there is not match, then we skip the entry
-            if closest_match['distance'] < args.distance or args.distance < 0.0:
-                # Inform we found a match
-                print ("Match found for TARGET entry: " + str(index))
-                 # # Insert current target entry to the results dataframe
-                # df_results = df_results.append(row, ignore_index=True)
- 
-                # If the key is provided, then only the fields that match the key will be appended. If the key is None, then all the fields will be appended
-                if args.key is not None:
-                    print ("Appending fields that match the key: " + args.key + " from SOURCE to TARGET")
-                    # Get the fields that match the key
-                    fields_to_append = df_source.filter(regex=args.key).columns
-                    for field in fields_to_append:
-                        row[field] = closest_match[field]
-                else:
-                    print ("Appending all fields from SOURCE to TARGET")
-                    # Append all the fields from SOURCE to the TARGET entry prepending the field name with the "source_"
-                    # This is done to avoid name collisions 
-                    for field in df_source.columns:
-                        if field != 'distance':
-                            row['source_' + field] = closest_match[field]
-                    # Add a new field to the TARGET entry with the distance between the TARGET and SOURCE entries
-                    row['distance'] = closest_match['distance']
-                # Append the TARGET entry to the results dataframe
-                df_results = df_results.append(row, ignore_index=True)
+            
+            # We check if the distance threshold is met. If args.distance < 0.0, then no filtering is applied
+            # Create a new dataframe with the entries that are within the threshold distance
+            if args.distance < 0.0:
+                df_source_filtered = df_source
             else:
+                df_source_filtered = df_source[df_source['distance'] < args.distance]
+
+            # If there are no matches, then we skip the entry
+            if df_source_filtered.empty:
                 print ("No match found for TARGET entry: " + str(index))
+                continue
+
+            # Check args.mode for options: closest, random (uninplemented), all
+            # If mode is closest, then we only append the closest match from df_source_filtered
+            if args.mode == 'closest':
+                # Get the closest match (single entry)
+                closest_match = df_source_filtered.iloc[0]
+                print ("Match found for TARGET entry: " + str(index))
+
+                for field in fields_to_append:
+                    row['source_' + field] = closest_match[field]
+
+
+                # Append the TARGET entry to the results dataframe
+                df_results = pd.concat([df_results, row.to_frame().T], ignore_index=True)
+
+            elif args.mode == 'all':
+                # Append all the matches from df_source_filtered to the TARGET entry
+                n = len(df_source_filtered.index)
+                print ("Appending [" + str(n) + "] matches from SOURCE to TARGET: " + str (index))
+                # Iterate over the matches
+                for index, match in df_source_filtered.iterrows():
+                    # If the key is provided, then only the fields that match the key will be appended. If the key is None, then all the fields will be appended
+                    for field in fields_to_append:
+                        row['source_' + field] = match[field]
+                    # Append the TARGET entry to the results dataframe
+                    df_results = pd.concat([df_results, row.to_frame().T], ignore_index=True)
+
 
         # Interrupt the execution if the SIGNAL was received
     except KeyboardInterrupt:
